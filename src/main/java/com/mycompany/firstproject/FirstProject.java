3236e9fe1cfe2654de78a86f82aae2de02112048
sudekirici <sudeekiricii@gmail.com>
2023-09-11 12:56:28 +0300

    first commit created.

---
+++ src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author S
+ */
+public class FirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
